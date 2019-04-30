1ab0b9e3af176101950395cc258cd3002a8d390c
Robert van Kooten <39274174+ThatRobVK@users.noreply.github.com>
2019-04-30 20:08:13 +0100

    Add basic .NET core console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TIL_Compression_using_Digital_Trees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
